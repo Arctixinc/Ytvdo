be5f082091f910ff98b7f07ccbd9cd0ab7d0c755
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2024-04-05 15:11:10 +0530

    Update send_video.py

--- mod/send_video.py
+++ mod/send_video.py
@@ -1,5 +1,5 @@
 from pyrogram import Client
-from Arctix import Arctix as app
+
 
 async def send_video(chat_id, video_path, thumbnail_path, duration, width, height, caption, progress=None):
     await app.send_video(
